e8456fbec3bc4537bf185e36915b8d1061cc4de2
Nejc Sušnik <nejc.susnik05@gmail.com>
2025-12-02 23:37:35 +0100

    one liner

--- 2025/d1/p2.hs
+++ 2025/d1/p2.hs
@@ -2,37 +2,16 @@ import Data.List
 import Data.Char
 import Data.String
 import Debug.Trace
+import Data.Bifunctor
 
-splitStr :: Char -> String -> [String]
-splitStr _ [] = []
-splitStr a [e]
-  | a == e = []
-  | otherwise = [[e]]
-splitStr a (e : f : xe)
-  | a == e = splitStr a (f : xe)
-  | a == f = [e] : splitStr a xe
-  | otherwise = (e : first) : rest
-  where
-    (first : rest) = splitStr a (f : xe)
-
-dirNumToNum :: String -> Int
-dirNumToNum s
-    | head s == 'L' = -(read (tail s) :: Int)
-    | head s == 'R' = read (tail s) :: Int
-    | otherwise = error "Invalid direction"
-
---pos res_sum chunks
-solve :: Int -> Int -> [Int] -> Int
-solve _ res_sum [] = res_sum
-solve pos_sum res_sum (this_pos : rest)
-    | rem (this_pos + pos_sum) 100 == 0 = solve (pos_sum + this_pos) (res_sum + 1) rest
-    | otherwise = solve (pos_sum + this_pos) res_sum rest
-
-splitChunk :: Int -> [Int]
-splitChunk 0 = []
-splitChunk n
-    | n < 0 = (-1) : splitChunk (n + 1)
-    | otherwise = 1 : splitChunk (n - 1)
-
-
-main = interact $ show . solve 50 0 . concat . map (splitChunk . dirNumToNum) . splitStr '\n'
+main = interact 
+    $ show 
+    . snd
+    . foldl (\(acc_pos, acc_cnt) x -> (acc_pos + x, if rem (acc_pos + x) 100 == 0 then acc_cnt + 1 else acc_cnt)) (50, 0)
+    . concat 
+    . map (
+        (\n -> if n < 0 then replicate (abs n) (-1) else replicate n 1)
+        . (\s -> if head s == 'L' then -(read (tail s) :: Int) else (read (tail s) :: Int))
+    ) 
+    . filter (\x -> length x /= 0)
+    . (unfoldr (\s -> if null s then Nothing else Just (bimap (\x -> x) (drop 1) (span (/= '\n') s))))
