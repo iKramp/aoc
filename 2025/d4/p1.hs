fef25e66e50f7362b52e791acaccda7ca42483cd
Nejc Sušnik <nejc.susnik05@gmail.com>
2025-12-04 18:01:33 +0100

    oopsie

--- 2025/d4/p1.hs
+++ 2025/d4/p1.hs
@@ -10,32 +10,30 @@ main = interact
     . filter (== 'x')
     . concat
     . (\(arr :: [String], (rows :: [Int], cols :: [Int])) -> 
-        foldl(\acc_arr_out iter ->
-            foldl (\acc_arr_c row -> 
-                foldl (\acc_arr col -> 
-                    if (acc_arr !! row !! col == '.') then
-                        acc_arr
-                    else if (4 > (length (filter (== '@')
-                        [
-                            acc_arr !! (row + 1) !! (col + 1),
-                            acc_arr !! (row + 1) !! (col + 0),
-                            acc_arr !! (row + 1) !! (col - 1),
-                            acc_arr !! (row + 0) !! (col - 1),
-                            acc_arr !! (row - 1) !! (col - 1),
-                            acc_arr !! (row - 1) !! (col + 0),
-                            acc_arr !! (row - 1) !! (col + 1),
-                            acc_arr !! (row + 0) !! (col + 1)
-                        ]
-                    ))) then 
-                        (take row acc_arr) ++ ([
-                            (take col (acc_arr !! row)) ++ ['x'] ++ (drop (col + 1) (acc_arr !! row))
-                        ]) ++ (drop (row + 1) acc_arr)
-                    else (
-                        acc_arr 
-                    )
-                ) acc_arr_c cols
-            ) acc_arr_out rows
-        ) arr [1..100]
+        foldl (\acc_arr_c row -> 
+            foldl (\acc_arr col -> 
+                if (acc_arr !! row !! col == '.') then
+                    acc_arr
+                else if (4 > (length (filter (/= '.')
+                    [
+                        acc_arr !! (row + 1) !! (col + 1),
+                        acc_arr !! (row + 1) !! (col + 0),
+                        acc_arr !! (row + 1) !! (col - 1),
+                        acc_arr !! (row + 0) !! (col - 1),
+                        acc_arr !! (row - 1) !! (col - 1),
+                        acc_arr !! (row - 1) !! (col + 0),
+                        acc_arr !! (row - 1) !! (col + 1),
+                        acc_arr !! (row + 0) !! (col + 1)
+                    ]
+                ))) then 
+                    (take row acc_arr) ++ ([
+                        (take col (acc_arr !! row)) ++ ['x'] ++ (drop (col + 1) (acc_arr !! row))
+                    ]) ++ (drop (row + 1) acc_arr)
+                else (
+                    acc_arr 
+                )
+            ) acc_arr_c cols
+        ) arr rows
     )
     . (\arr -> (arr, (enumFromTo 1 (length arr - 2), enumFromTo 1 (length (head arr) - 2))))
     . (\(arr, n) -> [replicate n '.'] ++ arr ++ [replicate n '.'])
